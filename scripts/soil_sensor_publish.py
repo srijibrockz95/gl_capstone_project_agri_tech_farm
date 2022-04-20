74109adc08783d6aa3386023e2c2bc7f70dc2863
sandeepkrishnan <sand.krishnan@gmail.com>
2022-04-20 17:20:42 +0530

    Added comments to the soil_sensor_publish.py file

--- scripts/soil_sensor_publish.py
+++ scripts/soil_sensor_publish.py
@@ -25,13 +25,11 @@ class AWS():
     # It will create the MQTT client for AWS using the credentials
     # Connect operation will make sure that connection is established between the device and AWS MQTT
     def __init__(self, client, certificate, private_key):
-        print(f'Client is ',client,' and certificate is ',certificate, ' and private key is ', private_key)
         self.client_id = client
         self.device_id = client
         self.cert_path = PATH_TO_CERT + "/" + certificate
         self.pvt_key_path = PATH_TO_CERT + "/" + private_key
         self.root_path = PATH_TO_CERT + "/" + "AmazonRootCA1.pem"
-        self.config_path = PATH_TO_CERT+"/"+"provisioning-data.json"
         self.myAWSIoTMQTTClient = AWSIoTPyMQTT.AWSIoTMQTTClient(self.client_id)
         self.myAWSIoTMQTTClient.configureEndpoint(ENDPOINT, 8883)
         self.myAWSIoTMQTTClient.configureCredentials(self.root_path, self.pvt_key_path, self.cert_path)
@@ -61,8 +59,6 @@ class AWS():
             self.myAWSIoTMQTTClient.publish(TOPIC, messageJson, 1)
             print("Published: '" + json.dumps(message) + "' to the topic: " + TOPIC)
 
-
-
     def disconnect(self):
         self.myAWSIoTMQTTClient.disconnect()
 
@@ -87,27 +83,13 @@ if __name__ == '__main__':
             thing_dict = json.loads(jsonObj)
             thing_list.append(thing_dict)
 
+    #Create device/sensor objects using the appropriate certificates and private keys
     for device in thing_list:
         print(device["ThingName"], type(device["ThingName"]))
         device["ThingName"] = AWS(device["ThingName"], device["ThingName"]+".pem.crt", device["ThingName"]+"pem.key")
-    '''
-    s3Client = boto3.client('s3')
-    response = s3Client.get_object(
-        Bucket='capstoneagritechfarm1',
-        Key='provisioning-data.json',
-    )
 
-    details = response["Body"].read().decode('utf-8')
-    pprint.pprint(details)
-    print(type(details))
-    mappingData = json.loads(details)
-    print(type(mappingData["mapping"]))
-    for device in mappingData["mapping"]:
-        print(device["ThingName"], type(device["ThingName"]))
-        device["ThingName"] = AWS(device["ThingName"], device["ThingName"]+"-cert.pem.crt", device["ThingName"]+"-private.pem.key")
-    '''
 
-    # Publish to the same topic in a loop forever for all devices
+    # Publish to the same topic in a loop forever for all devices/sensors
     loopCount = 0
     scheduler = sched.scheduler(time.time, time.sleep)
     now = time.time()
@@ -116,9 +98,7 @@ if __name__ == '__main__':
             for device in thing_list:
                 scheduler.enterabs(now + loopCount, 1, device["ThingName"].publish)
             loopCount += 1
-            print("The now + loopcount value is",now+loopCount)
             scheduler.run()
-            #time.sleep(5)
         except KeyboardInterrupt:
             for device in thing_list:
                 device["ThingName"].disconnect()
