1f8ec5579816afbbc5ffad9dc31ade251c5bf937
sandeepkrishnan <sand.krishnan@gmail.com>
2022-04-30 15:28:14 +0530

    Comments updated for installing nodered

--- scripts/EC2_Simulator_setup.sh
+++ scripts/EC2_Simulator_setup.sh
@@ -7,6 +7,12 @@ pip install AWSIoTPythonSDK
 pip install boto3
 pip install cloudpathlib
 pip install schedule
+
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt-get install -y nodejs build-essential
+sudo npm install -g --unsafe-perm node-red
+node-red
+
 aws configure
 python3 soil_sensor_publish.py
 
