07609cd82175a298bbb86f989b146094d7c556a2
Cathyrekhs <rekhacthomas@gmail.com>
2022-05-05 21:14:14 +0530

    import changes

--- scripts/dbsetup_main.py
+++ scripts/dbsetup_main.py
@@ -1,14 +1,13 @@
-from setup import *
+from dbsetup_utils import *
 import time
 
 # One time use while setting up tables.
-# dynamodb = boto3.resource('dynamodb', 'us-east-1')
+dynamodb = boto3.resource('dynamodb', 'us-east-1')
 sns_client = boto3.client('sns', region_name='us-east-1')
-# create_aggregate_data_table(dynamodb=dynamodb)
-# create_anomaly_data_table(dynamodb=dynamodb)
-# create_device_data_table(dynamodb=dynamodb)
-# time.sleep(60)
-# insert_device_data(dynamodb=dynamodb)
+create_aggregate_data_table(dynamodb=dynamodb)
+create_anomaly_data_table(dynamodb=dynamodb)
+create_device_data_table(dynamodb=dynamodb)
+time.sleep(60)
+insert_device_data(dynamodb=dynamodb)
 create_sns(sns_client=sns_client)
-# create_gsi_anomaly_table()
 print("Done..")
